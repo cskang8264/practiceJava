d52f6b54d1d64baef540eeed3a5d6b5cca3b240c
Daewon_Kang <cskang8264@naver.com>
2020-11-18 00:34:10 +0900

    fix

--- src/programmers/hash1.java
+++ src/programmers/hash1.java
@@ -7,14 +7,30 @@ public class hash1 {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         HashMap<String, Integer> hm = new HashMap<>();
-        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
-        for (String player : completion) hm.put(player, hm.get(player) - 1);
+        for (String player : participant) {
+            hm.put(player, hm.getOrDefault(player, 0) + 1);
+            System.out.println(hm);
+        }
+        System.out.println("-----------------------------------------------");
+        for (String player : completion) {
+            hm.put(player, hm.get(player) - 1);
+            System.out.println(hm);
+        }
 
         for (String key : hm.keySet()) {
             if (hm.get(key) != 0){
                 answer = key;
             }
         }
+        System.out.println("------------------------------------------------");
+        System.out.println(answer);
         return answer;
+    }
+
+    public static void main(String[] args) {
+        hash1 data = new hash1();
+        String[] participant= {"leo","kiki","eden"};
+        String[] completion = {"kiki","eden"};
+        data.solution(participant,completion );
     }
 }
